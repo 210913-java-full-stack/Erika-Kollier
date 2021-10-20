9ba469ae6b9800962ff8e210f7bb8bc35a9cc25a
Kollier Martin <kollierm@yahoo.com>
2021-10-20 18:04:10 -0400

    Erika's UI Files

---
+++ NewTrainUI/admin.js
@@ -0,0 +1,36 @@
+
+
+
+/**
+ * Logout Function
+ */
+function userLogout() {
+    localStorage.clear();
+    console.log("clicked");
+  }
+  
+/**  Add more logic? and create a logout button add eventlistener
+    When mapping through tickets for passenger add a cancel ticket button and a check in button
+    getTrainList function fetchs the list of all the train routes
+    Use a for loop ti essentially may through the array and populate the table
+    */
+
+/**
+ * Get method to receive list of train schedule & passengers
+ */
+  (async function getTrainList() {
+    let response = await fetch(
+      "http://lcoalhost:8080/Erika-Kollier/trainList"
+    );
+    let json = await response.json();
+
+    let table = document.getElementById("TrainRouteTable");
+
+    for (let element of json) {
+      let tr = table.insertRow(-1);
+      for (let key in element) {
+        let cell = tr.insertCell(-1);
+        cell.innerHTML = element[key];
+      }
+    }
+  })();
